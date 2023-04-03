317cbf4f73b4395881347e65e39dc335aaadbf32
lceCoIdMoon <wehuffman15@gmail.com>
2023-04-03 19:21:32 -0400

    updates

---
+++ nebula_editor/lib/nebula_ui.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:nebula_editor/nebula.dart';
+
+extension NebulaNodeUI on NebulaNode {
+  Widget getPropertiesUI(BuildContext context) => properties.map((key, value) => null)
+}
--- nebula_platform/lib/src/nebula_platform_base.dart
+++ nebula_platform/lib/src/nebula_platform_base.dart
@@ -1,22 +1,32 @@
+import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
 import 'package:json_rpc_2/json_rpc_2.dart';
-
-typedef Json = Map<String, dynamic>;
+import 'package:stream_channel/stream_channel.dart';
 
 class Nebula {
   final Socket _socket;
   late final Client _client;
 
   Nebula._(this._socket) {
-    _socket.map((event) => json.decode(utf8.decode(event)) as Json).listen(_onMessage);
-
-    var client = Client(_socket.cast<String>());
+    _client = Client(StreamChannel(_socket, _socket).transform(StreamChannelTransformer.fromCodec(Utf8Codec())));
+    _client.listen().then((value) => _socket.close());
   }
 
   static Future<Nebula> connectToDebugger(String key, {int port = 3590}) async {
     final socket = await Socket.connect('127.0.0.1', port);
     return Nebula._(socket);
   }
+
+  Future<void> disconnectFromDebugger() => _client.close();
+
+  Future<void> getNodeProperties(String guid) {}
+
+  Future<void> setNodeProperty(String guid, String path, dynamic value) {}
+}
+
+class NebulaNode {
+  final String guid;
+  final Map<String, dynamic> properties;
 }

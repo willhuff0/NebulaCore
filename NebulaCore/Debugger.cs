8456cc34bd0ce3e078431f0b29dcadeac042d8f4
willhuff0 <wehuffman15@gmail.com>
2023-04-04 15:44:41 -0400

    updates

--- NebulaCore/Debugger.cs
+++ NebulaCore/Debugger.cs
@@ -1,6 +1,14 @@
+using System.Net;
+using System.Net.Sockets;
+
 namespace NebulaCore;
 
-public class Debugger
+public static class Debugger
 {
-    
+    private static TcpListener _listener;
+
+    public static void StartServer()
+    {
+        _listener = new TcpListener(IPAddress.None, )
+    }
 }

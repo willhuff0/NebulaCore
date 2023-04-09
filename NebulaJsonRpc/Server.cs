ec750d35bf82e63c2d3eda802b8470cf7c56deee
willhuff0 <wehuffman15@gmail.com>
2023-04-09 11:21:06 -0400

    updates

---
+++ NebulaJsonRpc/Server.cs
@@ -0,0 +1,26 @@
+﻿using WatsonWebsocket;
+
+namespace NebulaJsonRpc;
+
+public class Server
+{
+    private WatsonWsServer _server;
+
+    public Server(string host, int port)
+    {
+        _server = new WatsonWsServer(host, port);
+        _server.MessageReceived += MessageReceived;
+        _server.Start();
+    }
+
+    private void MessageReceived(object? sender, MessageReceivedEventArgs args)
+    {
+        
+    }
+
+    public void Stop()
+    {
+        _server.Stop();
+        _server.Dispose();
+    }
+}
